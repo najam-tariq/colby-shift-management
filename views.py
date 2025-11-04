73fab99c33503ff1ab3874fa2829a846f449655a
ranamoeezhassan <rmhass26@colby.edu>
2025-11-04 16:50:22 -0500

    Added basic, static dashboard, signup, and signin pages

--- views.py
+++ views.py
@@ -9,3 +9,19 @@ load_dotenv()
 
 # Create a blueprint
 main_blueprint = Blueprint('main', __name__)
+
+@main_blueprint.route('/', methods=['GET', 'POST'])
+def shiftManagement():
+    
+    return render_template('dashboard.html')
+
+@main_blueprint.route('/login', methods=['GET', 'POST'])
+def shiftManagementLogin():
+    
+    return render_template('login.html')
+
+
+@main_blueprint.route('/signup', methods=['GET', 'POST'])
+def shiftManagementSignUp():
+    
+    return render_template('signup.html')
